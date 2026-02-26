ccd6cd92ada1d6afaf8c37cbb635906d8a4809cb
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-26 10:03:25 +0530

    add tailwind css

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,8 +17,16 @@ export default defineConfig({
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@utils": path.resolve(__dirname, "./src/utils"),
-      "@api": path.resolve(__dirname, "./src/api"),
-      "@ui": path.resolve(__dirname, "./src/ui"),
+        "@api": path.resolve(__dirname, "./src/api"),
+        "@ui": path.resolve(__dirname, "./src/ui"),
+      },
     },
-  },
-});
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:9933',
+          changeOrigin: true,
+        },
+      },
+    },
+  });
