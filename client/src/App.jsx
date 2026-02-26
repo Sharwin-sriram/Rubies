10f5d3449a9d54ba00b52ff9281be482a025f42b
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-26 10:09:06 +0530

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,74 @@
-import HomePage from "@pages/HomePage";
-import LandingPage from "@pages/LandingPage";
-import { Route, Routes } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// Pages
+import HomePage from './pages/HomePage';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import Register from './pages/Register';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import Profile from './pages/Profile';
+import Orders from './pages/Orders';
+import OrderDetail from './pages/OrderDetail';
+
+// Components
+import ProtectedRoute from './components/ProtectedRoute';
+import AuthWrapper from './components/AuthWrapper';
+import ErrorBoundary from './components/ErrorBoundary';
 
 export default () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/shop" element={<HomePage />} />
-      </Routes>
-    </>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <AuthProvider>
+          <CartProvider>
+            <AuthWrapper>
+              <div className="min-h-screen bg-gray-50">
+                <Routes>
+                  <Route path="/" element={<LandingPage />} />
+                  <Route path="/shop" element={<HomePage />} />
+                  <Route path="/products/:id" element={<ProductDetail />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/cart" element={<Cart />} />
+                  <Route path="/profile" element={
+                    <ProtectedRoute>
+                      <Profile />
+                    </ProtectedRoute>
+                  } />
+                  <Route path="/orders" element={
+                    <ProtectedRoute>
+                      <Orders />
+                    </ProtectedRoute>
+                  } />
+                  <Route path="/orders/:id" element={
+                    <ProtectedRoute>
+                      <OrderDetail />
+                    </ProtectedRoute>
+                  } />
+                  <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+              </div>
+            </AuthWrapper>
+            <ToastContainer
+              position="top-right"
+              autoClose={3000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
+          </CartProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 };
